4dc38cfd54da1dc39004936244c6b520860ebe63
mariannareis <mari.ounaum@gmail.com>
2011-05-18 15:48:40 -0300

    changed the static dir to a variable dir, according to the app

--- spec/get_files_spec.rb
+++ spec/get_files_spec.rb
@@ -1,16 +1,18 @@
 require ("getfiles")
 
-describe "Get files" do
+describe "GetFiles" do
 
   it "should return a array of all archives in a directory" do
+    dir = File.dirname(__FILE__)
     files = GetFiles.new
-    files.get_rb_files_in_a_directory("exemplo_site").should have(36).files
+    files.get_rb_files_in_a_directory(dir).should have(3).files
   end
 
   it "should read the first line of one file" do
+    dir = File.dirname(__FILE__)
     files = GetFiles.new
-    files.get_rb_files_in_a_directory("exemplo_site")
-    files.read_the_first_line_of_each_file_in_a_directory("exemplo_site").should == "class ApplicationController < ActionController::Base\n"
+    files.get_rb_files_in_a_directory(dir)
+    files.read_the_first_line_of_each_file_in_a_directory(dir).should == "require (\"getfiles\")\n"
   end
 
 end
